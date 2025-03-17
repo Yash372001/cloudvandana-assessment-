d272d966ca6ce51e3c7d7d1729b869fef4d5721f
Yash372001 <159689677+Yash372001@users.noreply.github.com>
2025-03-17 15:31:28 +0530

    Add files via upload

---
+++ Todo-list/index.js
@@ -0,0 +1,25 @@
+function addTask() {
+    let taskInput = document.getElementById("taskInput");
+    let taskText = taskInput.value.trim();
+    if (taskText === "") {
+        alert("Please enter a task!");
+        return;
+    }
+
+    let taskList = document.getElementById("taskList");
+
+    let li = document.createElement("li");
+    li.textContent = taskText;
+
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "X";
+    deleteBtn.classList.add("delete-btn");
+    deleteBtn.onclick = function () {
+        taskList.removeChild(li);
+    };
+
+    li.appendChild(deleteBtn);
+    taskList.appendChild(li);
+
+    taskInput.value = "";
+        }
